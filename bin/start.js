41a3eeb7e7306070f8542a4f626c6f85770a8fd9
lhy <haoyuli40@gmail.com>
2024-11-23 18:46:20 +0800

    完成登录，签到的接口

--- bin/start.js
+++ bin/start.js
@@ -8,15 +8,13 @@ const express = require('express');
 const detect = require('detect-port');
 const CryptoUtil = require('../lib/util/cryptoUtil');
 const cors = require('cors');
-const path = require('path');
-const { log } = require('console');
-// 定义数据目录路径
 const host = 'localhost';
+const runningSet = new Set();
+const runningPort = new Set();
 let peers = [];
-const dataDir = path.join(__dirname, 'data');
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
 const BASE_PORT = 3001;
 let currentPort = BASE_PORT; // 当前可用的端口
@@ -30,55 +28,61 @@ app.use(express.json());
 const isPortAvaliable = (port) => {
     const availablePort = detect(port);
     if (port === availablePort) {
-        return true
+        return true;
     } else {
-        return false
+        return false;
     }
 };
 
 // 登录 API：供学生登录
-app.post('/login', async (req, res) => {
+app.post('/login', (req, res) => {
     const { studentId, password } = req.body;
     let blockchain = new Blockchain(studentId);
     let blocks = blockchain.getAllBlocks();
     let flag = 0;
     for (const block of blocks) {
         for (const transaction of block.transactions) {
-            if (transaction.type === "register" && transaction.studentId === studentId && CryptoUtil.hash(password) !== transaction.passwordHash) {
-                res.status(400).json({ error: '密码错误' });
-            } else if (transaction.type === "register" && transaction.studentId === studentId && CryptoUtil.hash(password) === transaction.passwordHash) {
-                flag++
+            if (transaction.type === 'register' && transaction.studentId === studentId && CryptoUtil.hash(password) !== transaction.passwordHash) {
+                return res.status(400).json({ error: '密码错误' });
+            } else if (transaction.type === 'register' && transaction.studentId === studentId && CryptoUtil.hash(password) === transaction.passwordHash) {
+                flag++;
                 break;
             }
         }
         if (flag) break;
     }
     if (!flag) {
-        res.status(400).json({ error: '未找到账号' });
+        return res.status(400).json({ error: '未找到账号' });
     }
-    console.log(blocks, 77777);
-
     if (isNodeRunning) {
         // 如果已有节点运行，设置 peers 为上一个端口
         peers = [`http://localhost:${lastStartedPort}`];
-        return res.status(400).json({ error: 'Node already running', peers });
+        if (runningSet.has(studentId)) {
+            return res.status(400).json({ error: 'this node is already running'});
+        }
     }
     // 查找可用端口
     while (!isPortAvaliable(currentPort)) {
         currentPort++;
+        break;
     }
     let operator = new Operator(studentId, blockchain);
+    let wallet = operator.getWallet();
+    let address = operator.getAddressForWallet(wallet.id);
     let miner = new Miner(blockchain);
     let node = new Node(host, currentPort, peers, blockchain);
     let httpServer = new HttpServer(node, blockchain, operator, miner);
     isNodeRunning = true;
     lastStartedPort = currentPort; // 保存上一个启动的端口
     httpServer.listen(host, currentPort);
-    res.json({ status: 'success', publicKey: node.wallet.publicKey });
+    runningPort.add(currentPort);
+    console.log('running port:', runningPort);
+    runningSet.add(studentId);
+    return res.json({ status: 'success', port: currentPort, wallet: wallet,  address:address});
 });
 
 
-app.post('/register', async (req, res) => {
+app.post('/register', (req, res) => {
     const { studentId, password } = req.body;
     if (!studentId || !password) {
         return res.status(400).json({ error: 'Missing studentId or password' });
@@ -108,16 +112,18 @@ app.post('/register', async (req, res) => {
         if (!operator.getWallets().length) {
             let newWallet = operator.createWalletFromPassword(password);
             let newAddress = operator.generateAddressForWallet(newWallet.id);
-            let newTransaction = operator.createRegister(newWallet.id, newAddress, '', 0, newAddress, studentId, null, Date.now(), CryptoUtil.hash(password));
+            let newTransaction = operator.createRegister(newWallet.id, newAddress, 0, newAddress, studentId, null, Date.now(), CryptoUtil.hash(password));
             let transactionCreated = blockchain.addTransaction(Transaction.fromJson(newTransaction));
+            runningPort.add(currentPort);
+            console.log('running port:', runningPort);
             res.status(200).json({ message: 'Node started successfully', port: currentPort, wallet: newWallet, address: newAddress, transaction: transactionCreated });
         } else {
-            res.status(400).json({ error: '已注册过，请登录' });
+            return res.status(400).json({ error: '已注册过，请登录' });
         }
 
     } catch (error) {
         console.error('Error starting node:', error);
-        res.status(500).json({ error: 'Failed to start node' });
+        return res.status(500).json({ error: 'Failed to start node' });
     }
 });
 
